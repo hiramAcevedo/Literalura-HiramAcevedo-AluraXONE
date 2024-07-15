f491c81da2a8236c15839b6ee5488d1b5f741c45
hiramAcevedo <hiram.acevedo.lopez@gmail.com>
2024-07-15 16:08:08 -0600

    creating project

---
+++ src/main/java/com/hiramwoki/literalura_hiramAcevedo/LiteraluraHiramAcevedoApplication.java
@@ -0,0 +1,13 @@
+package com.hiramwoki.literalura_hiramAcevedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraHiramAcevedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraHiramAcevedoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiramwoki/literalura_hiramAcevedo/LiteraluraHiramAcevedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiramwoki.literalura_hiramAcevedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraHiramAcevedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
